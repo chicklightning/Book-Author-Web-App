3cfe30a140fbbc6b83b36fa922beac6f790b159a
Ellie <brooklyn.elle@gmail.com>
2016-02-25 15:57:34 -0500

    added author and book model classes

---
+++ src/WebApp1/Models/author.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp1.Models
+{
+    public class author
+    {
+        [ScaffoldColumn(false)]
+        public int AuthorID { get; set; }
+        [Required]
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set;  }
+
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+
+        public virtual ICollection<book> books { get; set; }
+    }
+}
